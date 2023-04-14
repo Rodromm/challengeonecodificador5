fbafe59749a511e4cca67ab5339bb08fa15c5ffb
rodrigo <barbas010289@gmail.com>
2023-04-13 21:14:18 -0600

    challenge codificador

---
+++ js/copy.js
@@ -0,0 +1,12 @@
+var boton = document.getElementById("btn-copiador");
+boton.addEventListener("click", copiarAlPortapapeles, false);
+function copiarAlPortapapeles() {
+  var enlace = document.getElementById("enlace");
+  var inputFalso = document.createElement("input");
+  inputFalso.setAttribute("value", enlace.innerHTML);
+  document.body.appendChild(inputFalso);
+  inputFalso.select();
+  document.execCommand("copy");
+  document.body.removeChild(inputFalso);
+  alert("Copiado al portapapeles!");
+}
